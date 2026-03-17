2edf7e6d06555f9e329c552dee824d8e35ffdd18
richosj <okm760596@gmail.com>
2026-03-17 17:20:14 +0900

    정리

--- src/js/modules/bottomSheet.js
+++
@@ -1,9 +0,0 @@
-/**
- * 하단 시트 열기/닫기 (bottomSheet + sheetHandle)
- */
-export function init() {
-	const bottomSheet = document.getElementById('bottomSheet');
-	const sheetHandle = document.getElementById('sheetHandle');
-	if (!bottomSheet || !sheetHandle) return;
-	sheetHandle.addEventListener('click', () => bottomSheet.classList.toggle('is-open'));
-}
